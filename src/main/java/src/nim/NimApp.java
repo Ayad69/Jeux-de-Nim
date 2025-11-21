2825aa4ae75cbe6c02da87eda5f6d5321d5c048c
Ayad69 <88985906+Ayad69@users.noreply.github.com>
2025-11-21 09:29:04 +0100

    Update NimApp.java
    Update class NimApp, gérer le cas où l'utilisateur veut prendre une allumette en dessous du nombre réel

--- src/main/java/src/nim/NimApp.java
+++ src/main/java/src/nim/NimApp.java
@@ -25,29 +25,43 @@ public class NimApp extends Application {
     public void start(Stage primarystage) throws Exception {
 
         // Scene 1
-
+        // titre
         Label titre = new Label("Bienvenue dans le jeux de Nim");
+        //parametrer la taille du titre
         titre.setStyle("-fx-font-size: 20");
+        //feedback
         Label feedback = new Label();
-
+        //nom du joueur 1
         Label nomJ = new Label("Nom joueur 1 :");
+        // field pour le joueur 1
         TextField nomJInput = new TextField();
+        // max taille largeur
         nomJInput.setMaxWidth(150);
+        //nom du joueur 1
         Label nomJ2 = new Label("Nom joueur 2 :");
+        // field pour le joueur 2
         TextField nomJInput2 = new TextField();
+        // max taille largeur
         nomJInput2.setMaxWidth(150);
+        // label
         Label label = new Label("\nEntrez un nombre d'allumettes (minimum 10) : ");
+        // field pour le nombre d'allumettes
         TextField nombre = new TextField();
+        // max taille largeur du field nombre
         nombre.setMaxWidth(100);
-
+    // bouton ok
         Button ok = new Button("Ok");
-
+        // vbox
         VBox vbox = new VBox(5);
         vbox.getChildren().addAll(titre,nomJ,nomJInput,nomJ2,nomJInput2, label, nombre,ok,feedback);
         vbox.setAlignment(Pos.CENTER);
-
+        // scene 1
         Scene scene = new Scene(vbox,700,500);
+
+
         //SCENE 2
+
+        //feedback pour le nombre d'allumettes
         Label allumettes = new Label();
         Label feedback2 = new Label();
         Label joueur = new Label();
@@ -60,8 +74,10 @@ public class NimApp extends Application {
 
         Button  joueurButton = new Button("Ok");
         Button  joueur2Button = new Button("Ok");
+
+        Label feedback3 = new Label();
         VBox vbox2 = new VBox(5);
-        vbox2.getChildren().addAll(allumettes,joueur,joueur1Input,joueurButton,joueur2,joueur2Input, joueur2Button,feedback2);
+        vbox2.getChildren().addAll(allumettes,joueur,joueur1Input,joueurButton,joueur2,joueur2Input, joueur2Button,feedback2,feedback3);
         vbox2.setAlignment(Pos.CENTER);
         Scene scene2 = new Scene(vbox2,700,500);
 
@@ -104,45 +120,59 @@ public class NimApp extends Application {
             }
         });
         joueurButton.setOnAction(e -> {
-          try{
-              int nbjoueur1 = Integer.parseInt(joueur1Input.getText());
-              if(1<=nbjoueur1 &&  nbjoueur1 <= 3) {
-                  nombreAllumettes -=  nbjoueur1;
-                  commande.afficherAllumettes(nombreAllumettes,allumettes);
-                  if(nombreAllumettes == 1) {
-                      feedback2.setText("Tu as perdu "+nomJoueur2);
-                      joueurButton.setDisable(true);
-                      joueur1Input.setDisable(true);
+            try {
+                int nbjoueur1 = Integer.parseInt(joueur1Input.getText());
+                if (1 <= nbjoueur1 && nbjoueur1 <= 3) {
+                    if(nombreAllumettes-nbjoueur1<=0){
+                        nombreAllumettes += nbjoueur1;
+                        feedback2.setDisable(true);
+                        feedback3.setText("Tu peux pas");
+                    }{
+                        nombreAllumettes -=  nbjoueur1;
+                        feedback2.setText(nomJoueur1+" a enlevé : "+nbjoueur1);
+                    }
 
-                      joueur2Button.setDisable(true);
-                      joueur2Input.setDisable(true);
-                      gagnant.setText("Le gagnant est "+nomJoueur1);
-                      primarystage.setScene(scene3);
+                    System.out.println("Nombre joueur 2 : " + nbjoueur1);
+                    System.out.println("Nombre enlever allumettes apres que  le joueur 2 a enlever " + nombreAllumettes);
+                    commande.afficherAllumettes(nombreAllumettes, allumettes);
+                    if (nombreAllumettes == 1) {
+                        feedback2.setText("Tu as perdu "+nomJoueur2);
+                        gagnant.setText("Le gagnant est "+nomJoueur1);
+                        joueur2Button.setDisable(true);
+                        joueurButton.setDisable(true);
+                        joueurButton.setDisable(true);
+                        primarystage.setScene(scene3);
+                    }
+                    joueur.setDisable(true);
+                    joueur1Input.setDisable(true);
+                    joueurButton.setDisable(true);
 
-                  }
-                  joueur2.setDisable(false);
-                  joueur2Input.setDisable(false);
-                  joueur2Button.setDisable(false);
+                    joueur2Input.setDisable(false);
+                    joueur2Button.setDisable(false);
 
-                  joueur.setDisable(true);
-                  joueur1Input.setDisable(true);
-                  joueurButton.setDisable(true);
-
-              }else {
-                  feedback2.setText("Entrez un nombre entre 1 et 3");
-              }
-          } catch (Exception ex) {
-              feedback2.setText("Entrez un nombre ! ");
-          }
+                } else {
+                    feedback2.setText("Entrez un nombre entre 1 et 3");
+                }
+            }catch (Exception ex) {
+                feedback2.setText("Entrez un nombre ! ");
+            }
              });
 
         joueur2Button.setOnAction(e -> {
            try {
                int nbjoueur2 = Integer.parseInt(joueur2Input.getText());
                if (1 <= nbjoueur2 && nbjoueur2 <= 3) {
+                   if(nombreAllumettes-nbjoueur2<=0){
+                       feedback2.setVisible(false);
+                       feedback3.setText("Tu peux pas");
+                       nombreAllumettes += nbjoueur2;
+                   }{
+                       nombreAllumettes -=  nbjoueur2;
+                       feedback2.setText(nomJoueur2+" a enlevé : "+nbjoueur2);
+                   }
+
                    System.out.println("Nombre joueur 2 : " + nbjoueur2);
-                   nombreAllumettes -= nbjoueur2;
-                   System.out.println("Nombre enlever allumettes apres que  le joueur 2 a elevler " + nombreAllumettes);
+                   System.out.println("Nombre enlever allumettes apres que  le joueur 2 a enlever " + nombreAllumettes);
                    commande.afficherAllumettes(nombreAllumettes, allumettes);
                    if (nombreAllumettes == 1) {
                        feedback2.setText("Tu as perdu "+nomJoueur1);
@@ -176,6 +206,15 @@ public class NimApp extends Application {
             feedback2.setText("");
             joueur1Input.clear();
             joueur2Input.clear();
+            nomJInput.clear();
+            nomJInput2.clear();
+
+
+            joueur.setDisable(false);
+            joueur1Input.setDisable(false);
+            joueurButton.setDisable(false);
+
+
 
             primarystage.setScene(scene);
         });
